88e32982cd960a24c729b058190a5ac174bdec30
Karol Bilicki <karol.bilicki@schibsted.com>
2020-09-05 19:08:00 +0200

    chore: terraform s3

--- s3.tf
+++ s3.tf
@@ -1,9 +1,40 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-state-bilicki"
+    region = "eu-west-2"
+    key = "s3/mortgage-calculator"
+  }
+}
+
 provider "aws" {
   region = "eu-west-2"
 }
 
-resource "aws_s3_bucket" "mortgage-calculator-bucket" {
-  bucket = "mortgage-calculator"
+resource "aws_s3_bucket" "terraform-state-bilicki" {
+
+  bucket = "terraform-state-bilicki"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+}
+
+resource "aws_s3_bucket" "mortgage-calculator-bilicki" {
+
+  bucket = "mortgage-calculator-bilicki"
 
   website {
     index_document = "index.html"
